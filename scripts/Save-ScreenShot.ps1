eaa7de205fde421707deb1ff5d313202d284f3e2
saggie <sagiyama2010@gmail.com>
2016-12-15 22:53:50 +0900

    Revise Save-Screenshot

--- scripts/Save-ScreenShot.ps1
+++ scripts/Save-ScreenShot.ps1
@@ -1,27 +1,18 @@
-$thisDirectoryPath = Split-Path $MyInvocation.MyCommand.Path -Parent
-Invoke-Expression (Join-Path $thisDirectoryPath 'Get-ActiveWindowRect.ps1') | Out-Null
-
 New-Module {
-  function Save-ScreenShot()
+  function Save-ScreenShot
   {
-    $outputFileName = "scrshot_" + [Convert]::ToString((Get-Date).Ticks, 16).SubString(0, 9) + ".png"
-    $outputFilePath = Join-Path (Join-Path $Env:UserProfile "Desktop") $outputFileName
-    
-    $rect = Get-ActiveWindowRect
-    
-    $dpi = 1.5 # TODO consider DPI
-    $x = [int]($rect.x * $dpi)
-    $y = [int]($rect.y * $dpi)
-    $width = [int]($rect.width * $dpi)
-    $height = [int]($rect.height * $dpi)
-    
-    Add-Type -AssemblyName System.Drawing
-    $bimap = New-Object System.Drawing.Bitmap($width, $height)
-    $graphics = [System.Drawing.Graphics]::FromImage($bimap)
-    
-    $graphics.CopyFromScreen($x, $y, 0, 0, $bimap.Size)
-    $graphics.Dispose()
-    $bimap.Save($outputFilePath) 
+    Add-Type -AssemblyName System.Windows.Forms
+
+    # press Alt+PrintScreen
+    [System.Windows.Forms.SendKeys]::SendWait("%{PRTSC}")
+
+    $clipboardImage = [Windows.Forms.Clipboard]::GetImage()
+    if ($clipboardImage)
+    {
+      $outputFileName = "scrshot_" + [Convert]::ToString((Get-Date).Ticks, 16).SubString(0, 9) + ".png"
+      $outputFilePath = Join-Path (Join-Path $Env:UserProfile "Desktop") $outputFileName
+      $clipboardImage.Save($outputFilePath)
+    }
   }
   Export-ModuleMember -Function Save-ScreenShot
 }
