ae0047441853d645515e3074390b4bac85288143
saggie <sagiyama2010@gmail.com>
2016-12-15 09:36:09 +0900

    Formatting

--- scripts/WindowRectGetter.cs
+++ scripts/WindowRectGetter.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 public class WindowRectGetter
 {
     [DllImport("user32.dll")]
-    //[return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetWindowRect(IntPtr hWnd, out WindowRectRaw lpRect);
 
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
 
-    public static WindowRect GetForegroundWindowRect() {
+    public static WindowRect GetForegroundWindowRect()
+    {
         IntPtr hwnd = GetForegroundWindow();
         var rawRect = new WindowRectRaw();
         GetWindowRect(hwnd, out rawRect);
@@ -33,7 +33,8 @@ public class WindowRect
     public int width;
     public int height;
 
-    public WindowRect(WindowRectRaw raw) {
+    public WindowRect(WindowRectRaw raw)
+    {
         this.x = raw.left;
         this.y = raw.top;
         this.width = raw.right - raw.left;
