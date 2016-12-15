7626ab5d4e6de876d2d601d5c5ce68a56a1d36cd
saggie <sagiyama2010@gmail.com>
2016-12-15 23:58:35 +0900

    1) Cleared clip-image in advance, 2) Kept current clip-text, 3) Added sleep for the completion of transferring data between clipboard.

--- scripts/Save-ScreenShot.ps1
+++ scripts/Save-ScreenShot.ps1
@@ -3,16 +3,29 @@ New-Module {
   {
     Add-Type -AssemblyName System.Windows.Forms
 
+    # save (push) clipboard
+    $clipText = [Windows.Forms.Clipboard]::GetText()
+
+    # clear clipboard
+    [Windows.Forms.Clipboard]::SetText(" ")
+    Start-Sleep -Milliseconds 100
+
     # press Alt+PrintScreen
     [System.Windows.Forms.SendKeys]::SendWait("%{PRTSC}")
+    Start-Sleep -Milliseconds 100
 
     $clipboardImage = [Windows.Forms.Clipboard]::GetImage()
+    Start-Sleep -Milliseconds 100
+    
     if ($clipboardImage)
     {
       $outputFileName = "scrshot_" + [Convert]::ToString((Get-Date).Ticks, 16).SubString(0, 9) + ".png"
       $outputFilePath = Join-Path (Join-Path $Env:UserProfile "Desktop") $outputFileName
       $clipboardImage.Save($outputFilePath)
     }
+    
+    # load (pop) cliptext
+    [Windows.Forms.Clipboard]::SetText($clipText)
   }
   Export-ModuleMember -Function Save-ScreenShot
 }
